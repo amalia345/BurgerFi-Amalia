aaf89d533f555f1aeba936a7743fe36dc3c498b9
Amalia Soto <75153007+amalia345@users.noreply.github.com>
2023-11-05 23:40:02 -0600

    finish log in

--- BurgerFi-Client/src/components/Login/Login.tsx
+++ BurgerFi-Client/src/components/Login/Login.tsx
@@ -1,19 +1,21 @@
 
-import { useState } from 'react';
+import { useState, FormEvent } from 'react';
 import { useNavigate } from 'react-router-dom';
+import './login.css'
+import mascot from '../../assets/burgerFiQueen.png'
 
 function Login() {
 
     const secretUser: string = 'Lomito'
     const secretPassword: string = '7456'
-
+    
     const navigate = useNavigate();
-
     const [username, setUsername] = useState('')
     const [password, setPassword] = useState('')
     const [error, setError] = useState('')
 
-    const handleLogin = () => {
+    const handleLogin = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
         if (username === secretUser) {
             if (password === secretPassword) {
                 navigate('/menu');
@@ -27,25 +29,33 @@ function Login() {
     }
 
     return (
-        <>
-            <h2>Login to BurgerFi</h2>
-            <input
-
-                placeholder='Write your user'
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-            >  </input>
-            <input
-                type='password'
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-            >  </input>
-
-
-
-            <button onClick={handleLogin}>Login</button>
-            <p>{error}</p>
-        </>
+        <main className='login-page'>
+            <header>
+                <h1>Burger <span>Queen</span></h1>
+            </header>
+            <section className='login-container'>
+                <figure>
+                    <img src={mascot} alt="" />
+                </figure>
+                <form className='login-Form' onSubmit={handleLogin}>
+                    <h2>Login to BurgerFi</h2>
+                    <input
+                        type='text'
+                        placeholder='Write your user'
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                    >  </input>
+                    <input
+                        type='password'
+                        placeholder='Password'
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                    >  </input>
+                    <button type='submit' >Login</button>
+                    <p>{error}</p>
+                </form>
+            </section>
+        </main>
     );
 }
 
